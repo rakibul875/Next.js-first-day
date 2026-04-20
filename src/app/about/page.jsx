cca89ed4479fab6af4484d3b3557704c5ad41287
rakibul875 <rakibulislamashik78@gmail.com>
2026-04-20 16:14:17 +0600

    add about and contact pages

---
+++ src/app/about/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const about = () => {
+    return (
+        <div>
+            <h1 className='text-5xl text-red-600'>Hello From about</h1>
+        </div>
+    );
+};
+
+export default about;
---
+++ src/app/contact/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const contact = () => {
+    return (
+        <div>
+            <h1>Hello from contact</h1>
+        </div>
+    );
+};
+
+export default contact;
